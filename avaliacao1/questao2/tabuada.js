bf193ac1e93ceee826b894303813879b4a5ab33e
Ariadny <ariadny.santos@escolar.ifrn.edu.br>
2022-06-13 22:36:39 -0300

    att

--- avaliacao1/questao2/tabuada.js
+++ avaliacao1/questao2/tabuada.js
@@ -17,17 +17,27 @@ function criar(){
 	var star = document.getElementById('star').value
 	var end = document.getElementById('end').value
 
+	//criando uma variavel para a tag tabela ja criada em meu html
 	var tabela = document.getElementById("table")
+	tabela.innerHTML = ""
+	// dado o "tamanho" da tabela pelo usuario, aqui ira começar a criação de fato da tabela segundo esses parametros
 	for(i=star; i<=end; i++){
 		var linha = document.createElement("tr")
+		//coluna do num que sera repetida em todas as linhas
 		var coluna = document.createElement("td")
+		//coluna do numero ao qual ele ira ser mult
 		var coluna2 = document.createElement("td")
+		//coluna que ira armazenar resultado da mult entre as duas colunas
 		var coluna3 = document.createElement("td")
 
+		//o texto da linha ficara vazio pois o que sera inserido nela vai ser uma tag e dessa forma não ira da erro no appendChild()
 		var textlinha = document.createTextNode("")
         linha.appendChild(textlinha)
+        /*com o .insertBefore eu expecifico a localização ao qual eu quero que minha tag fique desse caso toda linha
+        criada ficara abaixo da ultima "irma" que estiver em seu pai */
 		tabela.insertBefore(linha, tabela.children[end])
 
+		//aqui vai haver a criação de fato das colunas com os seus respectivos valores 
 		var textcolu1 = document.createTextNode(num)
 		coluna.appendChild(textcolu1)
 		linha.insertBefore(coluna, linha.children[0])
